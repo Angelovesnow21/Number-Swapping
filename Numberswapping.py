a7347fe074d831ea88ee0c7b28ff8bb0fd8aa37d
Angelovesnow21 <jessica.elaine8468@gmail.com>
2026-03-14 14:47:24 +0600

    Add files via upload

---
+++ Numberswapping.py
@@ -0,0 +1,12 @@
+# Take input values from user
+num1 = input("Enter a number:")
+num2 = input("Enter a another number:")
+
+# Swapping
+temp = num1
+num1 = num2
+num2 = temp
+
+# Displaying results after swapping
+print("value of num1 after swapping", num1)
+print("value of num2 after swapping", num2)
